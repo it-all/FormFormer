fa6c7eea1d9a311229a63e5c2a5bf6bfc513321f
GregCatalano <greg@it-all.com>
2017-07-30 08:52:09 -0400

    add fieldErrorMessages property, set and getter

--- src/Form.php
+++ src/Form.php
@@ -8,26 +8,32 @@ class Form extends NodeHolder
     /** @var  string. set to first field or first field with error */
     private $focusFieldId;
 
-    /** @var  string */
-    private $errorMessage;
-
     /** @var array  */
     private $attributes;
 
+    /** @var  string */
+    private $errorMessage;
+
+    /** @var array can be used to display specific errors at the top in case of long form */
+    private $fieldErrorMessages;
+
     const GENERAL_ERROR_MESSAGE = 'Submission Error';
 
-
-    /** Sending a non-empty $errorMessage causes $error to be true, even if there are no field errors, and overrides the GENERAL_ERROR_MESSAGE  */
+    /** Sending a non-empty $errorMessage causes an error to display, even if there are no field errors, and overrides the GENERAL_ERROR_MESSAGE
+     *
+     */
     public function __construct(array $nodes, array $attributes = [], string $errorMessage = '')
     {
         $this->attributes = $attributes;
         parent::__construct($nodes);
         $this->errorMessage = ''; // initialize
+        $this->fieldErrorMessages = []; // initialize
 
         $this->setFieldErrorsAndFocus($nodes);
         if (strlen($errorMessage) > 0) {
             $this->errorMessage = $errorMessage;
         }
+
     }
 
     // also validates incoming array to be Field or Fieldset objects
@@ -45,10 +51,17 @@ class Form extends NodeHolder
                     $this->focusFieldId = $node->getId();
                 }
 
-                if (!$this->hasError() && $node->getError()) {
-                    $this->errorMessage = self::GENERAL_ERROR_MESSAGE;
-                    $this->focusFieldId = $node->getId();
+                if ($node->getError()) {
+                    $this->$fieldErrorMessages[] = $node->getErrorMessage();
+                    if (!$this->hasError()) {
+                        $this->errorMessage = self::GENERAL_ERROR_MESSAGE;
+                        $this->focusFieldId = $node->getId();
+                    }
                 }
+//                if (!$this->hasError() && $node->getError()) {
+//                    $this->errorMessage = self::GENERAL_ERROR_MESSAGE;
+//                    $this->focusFieldId = $node->getId();
+//                }
 
             } else {
                 // Fieldset
@@ -79,5 +92,10 @@ class Form extends NodeHolder
     public function getErrorMessage()
     {
         return $this->errorMessage;
+    }
+
+    public function getFieldErrorMesssages()
+    {
+        return $this->fieldErrorMessages;
     }
 }
