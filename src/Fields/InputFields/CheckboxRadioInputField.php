ad0a7a426c9e14336a2cce496e9f5742bd7b9ab8
GregCatalano <greg@it-all.com>
2017-08-05 12:16:45 -0400

    no need to discern which type it is

--- src/Fields/InputFields/CheckboxRadioInputField.php
+++ src/Fields/InputFields/CheckboxRadioInputField.php
@@ -5,12 +5,9 @@ namespace It_All\FormFormer\Fields\InputFields;
 
 class CheckboxRadioInputField extends \It_All\FormFormer\Fields\InputField
 {
-    // note still necessary to include type=radio attribute
-    public function __construct(string $which = 'checkbox', string $label = '', array $attributes = [], string $errorMessage = '')
+    // note still necessary to include type=radio or type=checkbox attribute
+    public function __construct(string $label = '', array $attributes = [], string $errorMessage = '')
     {
-        if ($which != 'checkbox' && $which != 'radio') {
-            throw new \Exception('Invalid type $which');
-        }
         parent::__construct($label, $attributes, $errorMessage, false); // false to put label after field
     }
 }
